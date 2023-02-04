dc95f41274300a4a25ef4377bb251004a4602c73
akuliakuli <kabby889@gmail.com>
2023-02-04 14:24:07 +0400

    Added item factory

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -15,11 +15,13 @@ namespace Engine.Factories
             _standardGameItems = new List<GameItem>();
             _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+            _standardGameItems.Add(new GameItem(9001, "Rat tail", 1));
+            _standardGameItems.Add(new GameItem(9002, "Snakeskin", 2));
         }
         public static GameItem CreateGameItem(int itemTypeID)
         {
             GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
-            if (standardItem != null)
+           if(standardItem != null)
             {
                 return standardItem.Clone();
             }
