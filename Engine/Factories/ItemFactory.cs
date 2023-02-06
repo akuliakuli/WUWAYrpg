79de344248417c647e208324086b43a71d130cf8
akuliakuli <kabby889@gmail.com>
2023-02-06 16:13:41 +0400

    format chagnes

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace Engine.Factories
 {
-     public static class ItemFactory
+    public static class ItemFactory
     {
-        private static List<GameItem> _standardGameItems;
+        private static readonly List<GameItem> _standardGameItems = new List<GameItem>();
         static ItemFactory()
         {
-            _standardGameItems = new List<GameItem>();
             _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
-            _standardGameItems.Add(new GameItem(9001, "Snake eyes ", 1));
+            _standardGameItems.Add(new GameItem(9001, "Snake fang", 1));
             _standardGameItems.Add(new GameItem(9002, "Snakeskin", 2));
+            _standardGameItems.Add(new GameItem(9003, "Rat tail", 1));
             _standardGameItems.Add(new GameItem(9004, "Rat fur", 2));
             _standardGameItems.Add(new GameItem(9005, "Spider fang", 1));
             _standardGameItems.Add(new GameItem(9006, "Spider silk", 2));
