40631fd3cfed4249bcf45434faf874e48637372d
akuliakuli <kabby889@gmail.com>
2023-02-06 16:56:15 +0400

    added trader factory

---
+++ Engine/Factories/TraderFactory.cs
@@ -0,0 +1,38 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    public static class TraderFactory
+    {
+        private static readonly List<Trader> _traders = new List<Trader>();
+        static TraderFactory()
+        {
+            Trader elzar = new Trader("Elzar");
+            elzar.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            Trader farmerTed = new Trader("Farmer Brook");
+            farmerTed.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            Trader herbTheHerbalist = new Trader("Herbert the Herbalist");
+            herbTheHerbalist.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            AddTraderToList(elzar);
+            AddTraderToList(farmerTed);
+            AddTraderToList(herbTheHerbalist);
+        }
+        public static Trader GetTraderByName(string name)
+        {
+            return _traders.FirstOrDefault(t => t.Name == name);
+        }
+        private static void AddTraderToList(Trader trader)
+        {
+            if (_traders.Any(t => t.Name == trader.Name))
+            {
+                throw new ArgumentException($"There is already a trader named '{trader.Name}'");
+            }
+            _traders.Add(trader);
+        }
+    }
+}
