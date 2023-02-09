63f92f5a1922da95aba8ebef353713897d6a45b3
akuliakuli <kabby889@gmail.com>
2023-02-09 18:16:32 +0400

    added extension method

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
+using Engine.Shared;
 
 
 namespace Engine.Factories
@@ -47,24 +48,25 @@ namespace Engine.Factories
             foreach (XmlNode node in nodes)
             {
                 GameItem.ItemCategory itemCategory = DetermineItemCategory(node.Name);
+
                 GameItem gameItem =
                     new GameItem(itemCategory,
-                                 GetXmlAttributeAsInt(node, "ID"),
-                                 GetXmlAttributeAsString(node, "Name"),
-                                 GetXmlAttributeAsInt(node, "Price"),
+                                 node.AttributeAsInt("ID"),
+                                 node.AttributeAsString("Name"),
+                                 node.AttributeAsInt("Price"),
                                  itemCategory == GameItem.ItemCategory.Weapon);
                 if (itemCategory == GameItem.ItemCategory.Weapon)
                 {
                     gameItem.Action =
                         new AttackWithWeapon(gameItem,
-                                             GetXmlAttributeAsInt(node, "MinimumDamage"),
-                                             GetXmlAttributeAsInt(node, "MaximumDamage"));
+                                             node.AttributeAsInt("MinimumDamage"),
+                                             node.AttributeAsInt("MaximumDamage"));
                 }
                 else if (itemCategory == GameItem.ItemCategory.Consumable)
                 {
                     gameItem.Action =
                         new Heal(gameItem,
-                                 GetXmlAttributeAsInt(node, "HitPointsToHeal"));
+                                 node.AttributeAsInt("HitPointsToHeal"));
                 }
                 _standardGameItems.Add(gameItem);
             }
@@ -80,23 +82,6 @@ namespace Engine.Factories
                 default:
                     return GameItem.ItemCategory.Miscellaneous;
             }
-        }
-        private static int GetXmlAttributeAsInt(XmlNode node, string attributeName)
-        {
-            return Convert.ToInt32(GetXmlAttribute(node, attributeName));
-        }
-        private static string GetXmlAttributeAsString(XmlNode node, string attributeName)
-        {
-            return GetXmlAttribute(node, attributeName);
-        }
-        private static string GetXmlAttribute(XmlNode node, string attributeName)
-        {
-            XmlAttribute attribute = node.Attributes?[attributeName];
-            if (attribute == null)
-            {
-                throw new ArgumentException($"The attribute '{attributeName}' does not exist");
-            }
-            return attribute.Value;
         }
     }
 }
