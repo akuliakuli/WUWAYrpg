d8602cc7186aa3bcc71a63b62cc1f139980ec673
akuliakuli <kabby889@gmail.com>
2023-02-10 15:34:49 +0400

    game session changed to interact with players dexterity

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -94,7 +94,8 @@ namespace Engine.ViewModels
         #endregion
         public GameSession()
         {
-            CurrentPlayer = new Player("Scott", "Fighter", 0, 10, 10, 1000000);
+            int dexterity = RandomNumberGenerator.NumberBetween(3, 18);
+            CurrentPlayer = new Player("Scott", "Fighter", 0, 10, 10, dexterity, 1000000);
             if (!CurrentPlayer.Inventory.Weapons.Any())
             {
                 CurrentPlayer.AddItemToInventory(ItemFactory.CreateGameItem(1001));
